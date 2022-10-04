6fa4ac9aa4a5d301293d7273d62f337404d1b158
lay423 <junpang486@gmail.com>
2022-10-04 14:35:30 +0900

    CurrencyCnt2 수정

--- src/basic/ArrayTest.java
+++ src/basic/ArrayTest.java
@@ -1,2 +1,15 @@
-package basic;public class ArrayTest {
+package basic;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            array[i] = i+1;
+        }
+        for(int i=0; i<10;i++){
+            System.out.println(array[i]);
+        }
+    }
 }
--- src/basic/InsertionSort.java
+++
@@ -1,7 +0,0 @@
-package basic;
-
-public class InsertionSort {
-    public static void main(String[] args){
-        System.out.println("ok");
-    }
-}
