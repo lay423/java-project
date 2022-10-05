310143e50886fcd0e84c3272fc7de233d2bba261
lay423 <junpang486@gmail.com>
2022-10-05 14:49:50 +0900

    CodeUp 과제 1001~1010

--- src/java1004/codeUp10xx/Code1010.java
+++ src/java1004/codeUp10xx/Code1010.java
@@ -2,7 +2,7 @@ package java1004.codeUp10xx;
 
 import java.util.Scanner;
 
-public class Main {
+public class Code1010 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
