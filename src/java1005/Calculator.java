cfb4efaccfd5089c22419ccdf3bf6fe59da8e047
lay423 <junpang486@gmail.com>
2022-10-05 10:17:12 +0900

    CalculatorMain commit 1. Calculator 2. CalculatorMain

---
+++ src/java1005/Calculator.java
@@ -0,0 +1,19 @@
+package java1005;
+
+public class Calculator {
+    public void plus(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void minus(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public void multiple(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public void divide(int a, int b){
+        System.out.println(a / (double) b);
+    }
+}
---
+++ src/java1005/CalculatorMain.java
@@ -0,0 +1,12 @@
+package java1005;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.plus(2, 3);
+        cal.minus(2, 3);
+        cal.multiple(2, 3);
+        cal.divide(7, 3);
+
+    }
+}
