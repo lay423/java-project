a0507ef7af3a2b32fdb4ff4c900dd1788c158e0e
lay423 <junpang486@gmail.com>
2022-10-06 13:19:24 +0900

    isAlphabet 구현하기

---
+++ src/java1006/cntAlphabet/JudgeAlphabetMain.java
@@ -0,0 +1,13 @@
+package java1006.cntAlphabet;
+
+public class JudgeAlphabetMain {
+    public static void main(String[] args) {
+        JudgeAlphabet ja = new JudgeAlphabet();
+        System.out.println(ja.isAlphabet('a'));
+        System.out.println(ja.isAlphabet('ㅇ'));
+    /*
+        true
+        false
+     */
+    }
+}
