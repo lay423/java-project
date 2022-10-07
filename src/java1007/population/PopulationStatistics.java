54061c6dbb724c11ee5f2bb059b1aa21bfc593a6
lay423 <junpang486@gmail.com>
2022-10-07 11:45:48 +0900

    Do something 구현해보기

--- src/java1007/population/PopulationStatistics.java
+++ src/java1007/population/PopulationStatistics.java
@@ -1,12 +1,15 @@
 package java1007.population;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PopulationStatistics {
@@ -22,28 +25,21 @@ public class PopulationStatistics {
         }
         System.out.println(fileContents);
     }
-    public void readFileByLine(String filename) throws IOException {
+
+    //while문 안에서 String을 PopulationMove로 parsing하여 pml에 add합니다.
+    //루프가 끝나면 return 합니다.
+    public List<PopulationMove> readFileByLineV2(String filename) throws IOException {
+
+        List<PopulationMove> pml = new ArrayList<>();
         BufferedReader br = new BufferedReader(
                 new FileReader(filename));
         String s = "";
-        String s2;
-        while ((s2 = br.readLine()) != null) {
-            s += s2;
-            System.out.println(s2);
+
+        while ((s = br.readLine()) != null) {
+            PopulationMove pm = parse(s);
         }
         br.close();
-        System.out.println(s);
-    }
-
-    public void readByLine2(String filename) {
-        try(BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)){
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        return pml;
     }
 
 
@@ -55,9 +51,9 @@ public class PopulationStatistics {
 
     //data에서 시도를 찾아 Map에 저장하는 메소드
     public void setMapsByLine(String filename, PopultaionMoveMap popultaionMoveMap) {
-        try(BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)){
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)) {
             String line;
-            for (int i = 0; i < 1000; i++) {
+            for (int i = 0; i < 10; i++) {
                 line = br.readLine();
                 popultaionMoveMap.save(parse(line));
             }
@@ -66,28 +62,33 @@ public class PopulationStatistics {
         }
     }
 
+    public void createAFile(String filename) throws IOException {
+        File file = new File(filename);
+        file.createNewFile();
+    }
+
     public static void main(String[] args) throws IOException {
         String address = "2021_인구관련연간자료_20221006_11746.csv";
         PopulationStatistics populationStatistics = new PopulationStatistics();
+
+
+    /*  PopulationMoveMap 테스트
         PopultaionMoveMap popultaionMoveMap = new PopultaionMoveMap();
 
         //data에서 시도를 찾아 Map에 저장하는 메소드
         populationStatistics.setMapsByLine(address, popultaionMoveMap);
 
-        for (int i = 1; i < 1000; i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.println(popultaionMoveMap.findById(i));
         }
-    /*결과
-        1 ,11 ,11
-        2 ,11 ,11
-        3 ,11 ,11
-        4 ,11 ,11
-        5 ,11 ,11
-        6 ,11 ,11
-        7 ,11 ,41
-        8 ,11 ,44
-        9 ,11 ,48
-     */
+    */
+    /*  List테스트
+        List<PopulationMove> pml = populationStatistics.readFileByLineV2(address);
+        for (PopulationMove populationMove : pml) {
+            System.out.println(pml);
+        }
+    */
+        populationStatistics.createAFile("from_to.txt");
 
     }
 
