cd351e033db652557a1a4e1a8650c503a82ef54e
lay423 <junpang486@gmail.com>
2022-10-07 11:04:03 +0900

    시도 매핑

--- src/java1007/population/PopulationStatistics.java
+++ src/java1007/population/PopulationStatistics.java
@@ -55,11 +55,32 @@ public class PopulationStatistics {
         return new PopulationMove(fromSido, toSido);
     }
 
+    //data에서 시도를 찾아 Map에 저장하는 메소드
+    public void setMapsByLine(String filename, PopultaionMoveMap popultaionMoveMap) {
+        try(BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)){
+            String line;
+            for (int i = 0; i < 10; i++) {
+                line = br.readLine();
+                popultaionMoveMap.save(parse(line));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         String address = "2021_인구관련연간자료_20221006_11746.csv";
         PopulationStatistics populationStatistics = new PopulationStatistics();
+        PopultaionMoveMap popultaionMoveMap = new PopultaionMoveMap();
 
+        //data에서 시도를 찾아 Map에 저장하는 메소드
+        populationStatistics.setMapsByLine(address, popultaionMoveMap);
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(popultaionMoveMap.findById(i));
+        }
 
 
     }
+
 }
